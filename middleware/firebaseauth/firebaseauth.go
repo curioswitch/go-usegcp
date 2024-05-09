f180ce2b9a9ca67a7c6c217225799745ce1f2325
Choko <choko@curioswitch.org>
2024-05-09 12:45:15 +0900

    Use Forbidden status for auth handler, not Unauthorized (#2)

--- middleware/firebaseauth/firebaseauth.go
+++ middleware/firebaseauth/firebaseauth.go
@@ -12,7 +12,7 @@ import (
 // using Firebase Authentication ID tokens. Requests with a valid ID token as the
 // bearer token in the Authorization header will proceed, with the token content
 // accessible from context.Context via TokenFromContext or RawTokenFromContext.
-// Requests without a valid ID token will be rejected with a 401 Unauthorized
+// Requests without a valid ID token will be rejected with a 403 Forbidden
 // response.
 func NewMiddleware(fbAuth *auth.Client) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
@@ -37,19 +37,19 @@ type handler struct {
 func (m *handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	hdr := req.Header.Get("Authorization")
 	if hdr == "" {
-		http.Error(w, "missing authorization header", http.StatusUnauthorized)
+		http.Error(w, "missing authorization header", http.StatusForbidden)
 		return
 	}
 
 	token, ok := strings.CutPrefix(hdr, "Bearer ")
 	if !ok {
-		http.Error(w, "malformed authorization header", http.StatusUnauthorized)
+		http.Error(w, "malformed authorization header", http.StatusForbidden)
 		return
 	}
 
 	decoded, err := m.fbAuth.VerifyIDToken(req.Context(), token)
 	if err != nil {
-		http.Error(w, "invalid token", http.StatusUnauthorized)
+		http.Error(w, "invalid token", http.StatusForbidden)
 		return
 	}
 
